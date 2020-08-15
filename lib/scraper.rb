a3c3d2b4645089d3bafe3209ffc50f32030afd91
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-15 20:56:24 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,15 @@ require_relative './course.rb'
 
 class Scraper
   
+  
+  def get_page
+    Nokogiri::HTML(open("https://flatironschool.com"))
+  end
+
+  def get_courses
+    get_page.css(".title-oE5vT4")
+  end
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -13,6 +22,12 @@ class Scraper
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
       end
+    end
+  end
+  
+  def make_courses
+    get_courses.each do |course|
+      Course.new(course.text, "schedule", "description")
     end
   end
   
