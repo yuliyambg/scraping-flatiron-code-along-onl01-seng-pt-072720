ef0e1999e96f3b0bb37a5724d277b8cbc5ff8329
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-05 21:39:45 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,22 +1,40 @@
+
 class Course
- 
+  
   attr_accessor :title, :schedule, :description
- 
+  
   @@all = []
- 
-  def initialize()
-    # @title = title
-    # @schedule = schedule
-    # @description = description
+  
+  def initialize
     @@all << self
   end
- 
+  
   def self.all
     @@all
   end
- 
+  
   def self.reset_all
     @@all.clear
   end
- 
+
 end
+
+# class Course
+ 
+#   attr_accessor :title, :schedule, :description
+ 
+#   @@all = []
+ 
+#   def initialize
+#     @@all << self
+#   end
+ 
+#   def self.all
+#     @@all
+#   end
+ 
+#   def self.reset_all
+#     @@all.clear
+#   end
+ 
+# end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,36 +5,57 @@ require_relative './course.rb'
 
 class Scraper
   
-  
   def get_page
     Nokogiri::HTML(open("https://flatironschool.com"))
   end
 
-  def get_courses
-    get_page.css(".title-oE5vT4")
-  end
-  
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
-  
-  def make_courses
-    get_courses.each do |course|
-      c = Course.new
-      c.title = course.text
-      c.description = "description"
-      c.schedule = "schedule"
-    end
-  end
-  
+def get_courses
+  get_page.css(".title-oE5vT4")
 end
+
+def make_courses
+  get_courses.each do |course|
+    c = Course.new
+    c.title = course.text
+    c.description = "description"
+    c.schedule = "schedule"
+  end
+end
+
+end
+
+# class Scraper
+  
+  
+#   def get_page
+#     Nokogiri::HTML(open("https://flatironschool.com"))
+#   end
+
+#   def get_courses
+#     get_page.css(".title-oE5vT4")
+#   end
+  
+#   def print_courses
+#     self.make_courses
+#     Course.all.each do |course|
+#       if course.title && course.title != ""
+#         puts "Title: #{course.title}"
+#         puts "  Schedule: #{course.schedule}"
+#         puts "  Description: #{course.description}"
+#       end
+#     end
+#   end
+  
+#   def make_courses
+#     get_courses.each do |course|
+#       c = Course.new
+#       c.title = course.text
+#       c.description = "description"
+#       c.schedule = "schedule"
+#     end
+#   end
+  
+# end
 
 
 
